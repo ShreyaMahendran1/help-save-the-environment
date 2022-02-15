270c34273193d697b6e7ab3fb55f1bcff01fefba
ShreyaMahendran1 <84270576+ShreyaMahendran1@users.noreply.github.com>
2022-02-15 13:57:25 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function go1(){
+    window.location.replace("1.html")
+}
+
+function go2(){
+    window.location.replace("2.html")
+}
+
+function go3(){
+    window.location.replace("3.html")
+}
+
+function go4(){
+    window.location.replace("4.html")
+}
+
+function go5(){
+    window.location.replace("5.html")
+}
+function go_back(){
+    window.location.replace("index.html")
+}
